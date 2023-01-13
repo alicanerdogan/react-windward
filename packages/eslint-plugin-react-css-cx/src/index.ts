ac62031b05d3d22095ea1f9b4fa16ce8de71f113
Alican <aerdogan07@gmail.com>
2023-01-13 20:27:48 +0100

    fix: Correct import for rules

--- packages/eslint-plugin-react-css-cx/src/index.ts
+++ packages/eslint-plugin-react-css-cx/src/index.ts
@@ -4,7 +4,10 @@ import recommended from "./configs/recommended";
 import elementClassNameUse from "./rules/element-class-name-use";
 import parentLevelClassUse from "./rules/parent-level-class-use";
 
-export const rules = [elementClassNameUse, parentLevelClassUse];
+export const rules = {
+  "element-class-name-use": elementClassNameUse,
+  "parent-level-class-use": parentLevelClassUse,
+};
 
 export const configs = {
   all,
