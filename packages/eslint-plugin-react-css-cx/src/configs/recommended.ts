b1819eef562e2ed8453708ca7425b9b760b11bc1
Alican <aerdogan07@gmail.com>
2023-01-14 19:25:10 +0100

    feat: Recommended rules severity has changed

--- packages/eslint-plugin-react-css-cx/src/configs/recommended.ts
+++ packages/eslint-plugin-react-css-cx/src/configs/recommended.ts
@@ -2,7 +2,7 @@ const config = {
   plugins: ["react-css-cx"],
 
   rules: {
-    "react-css-cx/element-class-name-use": "warn",
+    "react-css-cx/element-class-name-use": "off",
     "react-css-cx/parent-level-class-use": "warn",
   },
 };
